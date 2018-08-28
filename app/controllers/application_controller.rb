dfe90e49acfc9db39ac597800ce6d13fdd9cb52a
madtab7 <madtab7@gmail.com>
2018-08-28 15:45:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/team' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+
+      member_info = params[:team][:member]
+
+      @members =  member_info.map do |param|
+        Member.new(name: param[:name], power: param[:power], bio: param[:bio])
+      end
+
+
+      erb :team
+    end
 
 end
