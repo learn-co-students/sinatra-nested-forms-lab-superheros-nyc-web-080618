b2f3a4bc1cae1e4f3d82e99ac46b31e21565bfb8
github username <github email address>
2018-08-28 15:30:12 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    post '/team' do
+      @team = Team.new(params[:team][:name],params[:team][:motto])
+      params[:team][:heroes].each do |hero|
+        Hero.new(hero[:name],hero[:power],hero[:bio])
+      end
+      @heroes = Hero.all
+      erb :team
+    end
 
 end
